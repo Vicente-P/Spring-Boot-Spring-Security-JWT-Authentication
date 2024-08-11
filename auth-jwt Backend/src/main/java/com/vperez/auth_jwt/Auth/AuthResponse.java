7b290f2524a150ad569d7fed380fa33bcba25588
Vicente Joaquin Pérez Castro <vperezc18@gmail.com>
2024-08-11 19:07:59 -0400

    Backend para login y registro con Spring boot, jwt, spring security y mongodb

---
+++ auth-jwt Backend/src/main/java/com/vperez/auth_jwt/Auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.vperez.auth_jwt.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token;
+}
---
+++ auth-jwt Backend/src/main/java/com/vperez/auth_jwt/User/Role.java
@@ -0,0 +1,7 @@
+package com.vperez.auth_jwt.User;
+
+public enum Role {  
+    ADMIN,
+    USER
+
+}
